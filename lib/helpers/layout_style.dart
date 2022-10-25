b799e47a20fcf0815996adb409c9cf039d2a3c08
amrilrisman <amrilrisman@gmail.com>
2022-10-25 22:54:03 +0700

    add folder struktur dan menambhaknan theme style aplikasi

---
+++ lib/helpers/layout_style.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LayoutMargin {
+  static EdgeInsets marginHorizontal20 =
+      const EdgeInsets.symmetric(horizontal: 20);
+  static EdgeInsets marginVertical20 =
+      const EdgeInsets.symmetric(horizontal: 20);
+  static EdgeInsets marginTop20 = const EdgeInsets.only(top: 20);
+  static EdgeInsets marginBottom20 = const EdgeInsets.only(bottom: 20);
+  static EdgeInsets marginLeft20 = const EdgeInsets.only(left: 20);
+  static EdgeInsets marginRight20 = const EdgeInsets.only(right: 20);
+}
---
+++ lib/helpers/text_style.dart
@@ -0,0 +1,26 @@
+import 'package:digital_sport/helpers/color_style.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FontStyle {
+  static TextStyle h1 = GoogleFonts.beVietnamPro(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    color: ColorApp.txColorPrimary,
+  );
+  static TextStyle p1 = GoogleFonts.beVietnamPro(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: ColorApp.txColorsecondary2,
+  );
+  static TextStyle p2 = GoogleFonts.beVietnamPro(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    color: ColorApp.txColorsecondary2,
+  );
+  static TextStyle d1 = GoogleFonts.beVietnamPro(
+    fontSize: 10,
+    fontWeight: FontWeight.w500,
+    color: ColorApp.txColorsecondary2,
+  );
+}
